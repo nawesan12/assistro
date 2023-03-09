ee21e372d955e9e837a0221f10631f7f07bf9e8e
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-03-09 01:16:01 -0300

    animation for logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
-import "@/styles/home.css"
-
 import Image from "next/image"
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="main-home">
-      <p className="underline">Hola</p>
-      <Image src="/assistro.png" width={500} height={100} alt="Assistro" />
+    <main className="flex flex-col items-center justify-evenly">
+      <Image className="puff-in-center" src="/assistro.png" width={500} height={100} alt="Assistro" />
+
+      <nav>
+        <ul>
+          <li>
+            <Link href="/register/teacher">Soy profesor</Link>
+          </li>
+          <li>
+            <Link href="/register/student">Soy estudiante</Link>
+          </li>
+        </ul>
+      </nav>
     </main>
   )
 }

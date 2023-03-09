18eee7e0ebafc51c3fdfa92b14ef66b5bb9c4938
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-03-09 01:35:42 -0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,15 @@ import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-evenly">
+    <main className="h-screen flex flex-col items-center justify-evenly">
       <Image className="puff-in-center" src="/assistro.png" width={500} height={100} alt="Assistro" />
 
       <nav>
         <ul>
-          <li className="p-4 rounded-md border-2 border-l-indigo-300 my-4 bg-white">
+          <li className="p-4 px-8 rounded-md border-2 border-l-indigo-300 my-4 bg-white">
             <Link href="/register/teacher">Soy profesor</Link>
           </li>
-          <li className="p-4 rounded-md border-2 border-l-indigo-300 my-4 bg-white">
+          <li className="p-4 px-8 rounded-md border-2 border-l-indigo-300 my-4 bg-white">
             <Link href="/register/student">Soy estudiante</Link>
           </li>
         </ul>
